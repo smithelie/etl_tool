b76dffb369c00c01e7ee71e88a75bdf17b117501
Smith Elie <smithelie@tutanota.com>
2024-04-21 18:51:27 -0400

    Fleshed out documentation in test suite.

--- test_extract.py
+++ test_extract.py
@@ -27,15 +27,27 @@ class TestFileExtract(unittest.TestCase):
     path_to_json_data = '/home/smith/Development/ds5010/github/ds5010/etl_tool/data/json.json' # user must update
 
     def test_init_with_file_path(self):
+        '''
+        Tests the initialization of the FileExtract class
+        Ensure attribute assignment works as expected
+        '''
         file_extractor = FileExtract(self.path_to_tabular_data)
         self.assertEqual(self.path_to_tabular_data, file_extractor.file_path)
 
     def test_read_tabular(self):
+        '''
+        Tests the read_tabular method of the FileExtract class
+        Ensure tabular data is read and contains the expected number of rows and columns
+        '''
         file_extractor = FileExtract()
         tabular_data = file_extractor.read_tabular(delimiter=',', file_path=self.path_to_tabular_data)
         self.assertEqual((32, 12), tabular_data.shape)  # expected vs actual
 
     def test_read_json(self):
+        '''
+        Tests the read_json method of the FileExtract
+        Ensure the extracted JSON contains the expected keys
+        '''
         file_extractor = FileExtract()
         json_data = file_extractor.read_json(file_path=self.path_to_json_data)
         self.assertEqual(['id', 'name', 'email', 'address', 'orders'], list(json_data.keys()))  # expected vs actual
@@ -51,19 +63,35 @@ class TestDatabaseExtract(unittest.TestCase):
     connection_url = 'sqlite://///home/smith/Development/ds5010/github/ds5010/etl_tool/data/database.db'  # user must update
 
     def test_init_with_conn_str(self):
+        '''
+        Tests the init_with_conn_str method of the DatabaseExtract class
+        Ensures that when supplied a connection_str, the type of connection is pyodbc
+        '''
         database_extractor = DatabaseExtract(connection_str=self.connection_str)
         self.assertEqual('pyodbc', database_extractor.connection_type)  # expected vs actual. Assignment test.
 
     def test_init_with_conn_url(self):
+        '''
+        Tests the init_with_conn_url method of the DatabaseExtract class
+        Ensures that when supplied a connection_url, the type of connection is sqlalchemy
+        '''
         database_extractor = DatabaseExtract(connection_url=self.connection_url)
         self.assertEqual('sqlalchemy', database_extractor.connection_type)  # expected vs actual. Assignment test.
 
     def test_query_with_conn_str(self):
+        '''
+        Tests the query_with_conn_str method of the DatabaseExtract class
+        Ensure the queried data has the expected number of rows and columns
+        '''
         database_extractor = DatabaseExtract(connection_str=self.connection_str)
         data_via_pyodbc = database_extractor.query('SELECT * FROM people LIMIT 3')
         self.assertEqual((3, 3), data_via_pyodbc.shape)
 
     def test_query_with_conn_url(self):
+        '''
+        Tests the query_with_conn_str method of the DatabaseExtract class
+        Ensure the queried data has the expected number of rows and columns
+        '''
         database_extractor = DatabaseExtract(connection_url=self.connection_url)
         data_via_sql_alchemy = database_extractor.query('SELECT * FROM people LIMIT 5')
         self.assertEqual((5, 3), data_via_sql_alchemy.shape)
@@ -82,14 +110,26 @@ class TestAPIExtract(unittest.TestCase):
     path_to_openweather_config_file = '/home/smith/Development/ds5010/Secure/open_weather_credentials.json' # user must update
 
     def test_init(self):
+        '''
+        Test the initialization of the TestAPIExtract class.
+        Ensures the expected keys are present from the config_file used in initialization.
+        '''
         api_extractor = APIExtract(config=self.path_to_reddit_config_file)
         self.assertEqual(['client_id', 'client_secret'], list(api_extractor.config.keys()))
 
     def test_load_config(self):
+        '''
+        Test the loading of a config file.
+        Ensures the expected keys are present from the input config file.
+        '''
         api_extractor = APIExtract(config=self.path_to_reddit_config_file)
         self.assertIsInstance(api_extractor.load_config(self.path_to_reddit_config_file), dict)
 
     def test_fetch_data(self):
+        '''
+        Test the fetch_data method of the TestAPIExtract class
+        Ensures that, with a valid set of credentials, the data returned from the API endpoint contains the expected keys.
+        '''
         api_extractor = APIExtract(config=self.path_to_openweather_config_file)
         params = {'q': 'Boston', 'units': 'imperial'}
         df = api_extractor.fetch_data(url='https://api.openweathermap.org/data/2.5', command='/weather', params=params)
@@ -98,6 +138,10 @@ class TestAPIExtract(unittest.TestCase):
              'cod'], list(df.keys()))
 
     def test_get_top_n_reddit_posts(self):
+        '''
+        Test the get_top_n_reddit_posts method of the TestAPIExtract class
+        Ensures that, with a valid set of credentials, the data returned from the API endpoint contains the expected number of rows and columns.
+        '''
         api_extractor = APIExtract(config=self.path_to_reddit_config_file)
         top_10_reddit_posts = api_extractor.get_top_n_reddit_posts(sub='python')
         self.assertEqual((10, 4), top_10_reddit_posts.shape)
--- test_load.py
+++ test_load.py
@@ -22,6 +22,10 @@ class TestLoader(unittest.TestCase):
     output_path_for_loader = '/home/smith/Development/ds5010/github/ds5010/etl_tool/data/outbox/data_written_by_file_loader.csv' # user must update
 
     def test_init(self):
+        '''
+        Tests the initialization of a TestLoader object
+        Ensures that the attribute assignment works as expected.
+        '''
         # pass some raw data to an instance of the Transformer class for testing
         file_extractor = FileExtract()
         raw_tabular_data = file_extractor.read_tabular(file_path=self.path_to_tabular_data_with_array, delimiter=',')
@@ -31,6 +35,9 @@ class TestLoader(unittest.TestCase):
         self.assertEqual((4, 2), file_loader.data.shape)
 
     def test_write_to_file(self):
+        '''
+        Tests the writing of a dataset in a TestLoader object to the file system
+        '''
         file_extractor = FileExtract()
         raw_tabular_data = file_extractor.read_tabular(file_path=self.path_to_tabular_data_with_array, delimiter=',')
         file_loader = Loader(data=raw_tabular_data)
